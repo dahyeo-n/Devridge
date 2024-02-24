2c17b5f38f117506a53eab6a6020509a015f4a62
andan <andante1104@naver.com>
2024-02-24 17:36:31 +0900

    docs & fix : db.json location 의 속성 [locationName -> name] 으로 수정 | refactor : location 데이터 한번에 받아와서 변수 filTerArray에 저장하는 로직 리팩토링

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Map, MapMarker, useKakaoLoader } from 'react-kakao-maps-sdk';
 import axios from 'axios';
 import Header from 'components/commons/Header';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Main() {
   const [loading, error] = useKakaoLoader({
@@ -10,24 +10,21 @@ function Main() {
     // ...options // 추가 옵션
   });
 
-  const [mockdata, setMockData] = useState([]);
+  const [mockData, setMockData] = useState([]);
 
-  const fetchdata = async () => {
+  const fetchData = async () => {
     const { data } = await axios.get(`${process.env.REACT_APP_SERVER_URL}`);
-
     setMockData(data);
-
     return data;
   };
 
   useEffect(() => {
-    fetchdata();
+    fetchData();
   }, []);
 
+  const filTerArray = mockData.filter((data) => data).map((data) => data.location);
+  console.log(filTerArray);
   const navi = useNavigate();
-
-  const array = mockdata.filter((data) => data);
-  const filTerArray = array.map((data) => data.location);
 
   return (
     <>
@@ -37,18 +34,18 @@ function Main() {
         {/* 지도 나타내는 부분 */}
 
         <Map center={{ lat: 37.50232863613739, lng: 127.04444701396942 }} style={{ width: '50%', height: '360px' }}>
-          {filTerArray.map((position, index) => (
+          {filTerArray.map((position) => (
             <MapMarker
-              key={`${position.locationName}-${position.latlng}`}
-              position={position.latlng} // 마커를 표시할 위치
+              key={`${position.name}-${position.latlng}`}
+              position={position.latlng} // position :  마커를 표시할 위치
               image={{
                 src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png', // 마커이미지의 주소입니다
                 size: {
                   width: 24,
                   height: 35
-                } // 마커이미지의 크기입니다
+                } // size :   마커이미지의 크기입니다
               }}
-              title={position.locationName} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+              title={position.name} // title :  마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
             />
           ))}
         </Map>
@@ -56,7 +53,7 @@ function Main() {
         <article>
           <figure>
             {/* 사용자 글 리스트를 나타내는 부분    */}
-            {mockdata.map((data) => (
+            {mockData.map((data) => (
               <div key={data.password}>
                 <h1>{data.reviewTitle}</h1>
                 <small>{data.nickname}</small>
