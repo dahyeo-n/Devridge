121c73f1fafed558c2e5770c1ce71bf15cf13649
andan <andante1104@naver.com>
2024-02-25 20:04:50 +0900

    docs : Main.jsx 필요없는 console.log  제거 || refactor : 카카오 지도에 마커를 표시하는 부분 리팩토링

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -27,12 +27,10 @@ function Main() {
 
   const getReview = async () => {
     const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}`);
-    console.log(response.data);
     return response.data;
   };
 
   const { isLoading, isError, data } = useQuery('reviews', getReview);
-  console.log(data);
 
   if (isLoading) {
     console.log('로딩중입니다.');
@@ -44,6 +42,7 @@ function Main() {
     // isError를 사용하여 error가 발생할 때 화면을 랜더링합니다.
     return <div>오류!!!!</div>;
   }
+  const filterMapMarkerData = data.filter((data) => data).map((data) => data.location);
 
   return (
     <>
@@ -53,23 +52,20 @@ function Main() {
         {/* 지도 나타내는 부분 */}
 
         <Map center={{ lat: 37.50232863613739, lng: 127.04444701396942 }} style={styleMap}>
-          {data
-            .filter((data) => data)
-            .map((data) => data.location)
-            .map((position) => (
-              <MapMarker
-                key={`${position.name}-${position.latLng}`}
-                position={position.latLng} // position :  마커를 표시할 위치
-                image={{
-                  src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png', // 마커이미지의 주소입니다
-                  size: {
-                    width: 24,
-                    height: 35
-                  } // size :   마커이미지의 크기입니다
-                }}
-                title={position.name} // title :  마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-              />
-            ))}
+          {filterMapMarkerData.map((position) => (
+            <MapMarker
+              key={`${position.name}-${position.latLng}`}
+              position={position.latLng} // position :  마커를 표시할 위치
+              image={{
+                src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png', // 마커이미지의 주소입니다
+                size: {
+                  width: 24,
+                  height: 35
+                } // size :   마커이미지의 크기입니다
+              }}
+              title={position.name} // title :  마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            />
+          ))}
         </Map>
 
         <StDevRidgeReplyList>
