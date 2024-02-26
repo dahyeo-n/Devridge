315f8d8088b348c8d34c370bb676bfd1979532b2
andan <andante1104@naver.com>
2024-02-26 16:20:39 +0900

    style : reviewData.js 변경 무

--- src/api/reviewData.js
+++ src/api/reviewData.js
@@ -1,4 +1,3 @@
-// reviews.js :  reviews관련 데이터를 불러오는  api를 관리할 파일
 import axios from 'axios';
 export const getReviewData = async () => {
   const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}`);
