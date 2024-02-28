687ded5b48b261025abb7234a6440c0f77c14205
baram55 <asd7973@gmail.com>
2024-02-28 10:36:26 +0900

    Feat : reviewSlice.js 사용하지 않는 코드 삭제

--- src/store/modules/reviewSlice.js
+++ src/store/modules/reviewSlice.js
@@ -10,24 +10,10 @@ const reviewSlice = createSlice({
   reducers: {
     getReview: (state, action) => {
       state.review = action.payload;
-    },
-    addReview: (state, action) => {
-      state.review.push(action.payload);
-    },
-    delReview: (state, action) => {
-      state.review.filter((item) => item.id !== action.payload.id);
-    },
-    updateReview: (state, action) => {
-      state.review.map((item) => {
-        if (item.id === action.payload.id) {
-          return { ...item, ...action.payload };
-        }
-        return item;
-      });
     }
   }
 });
 
-export const { getReview, addReview, delReview, updateReview } = reviewSlice.actions;
+export const { getReview } = reviewSlice.actions;
 
 export default reviewSlice.reducer;
